706d48cef3190bf1c32813858783285c255556fd
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2023-11-18 21:47:57 +0100

    Refactor - enable multible instances - introduce setting for alternating interval - Remove SocketNotification handling and therefore also the node_helper.js (fixes #4) - Enable translations

--- MMM-learnlanguage.js
+++ MMM-learnlanguage.js
@@ -5,126 +5,116 @@
 Module.register("MMM-learnlanguage", {
   // Default module config.
   defaults: {
-    nextWordInterval: 90000, // interval to fetch a new wordpair
     language: "swedish", // name of CSV file without extension
-    text: "vertalen",
-    translatedtext: "translate",
+    nextWordInterval: 12 * 60 * 60 * 1000, // interval to switch to wordpair
     showpair: "alternating", // alternating or showboth
-    wordpaircssclassname: "bright xlarge"
+    toggleInterval: 10 * 1000, // if "alternating" toggle time between own language word and foreign language word
+    wordpaircssclassname: "bright large"
   },
 
   // Define required translations.
-  // getTranslations: function() {
-  //	return {
-  //    		en: "translations/en.json",
-  //    		nl: "translations/nl.json",
-  //	};
-  // },
+  getTranslations() {
+    return {
+      de: "translations/de.json",
+      en: "translations/en.json",
+      nl: "translations/nl.json"
+    };
+  },
 
   // Define start sequence.
-  start() {
-    Log.info(`Starting module: ${this.name}`);
-    this.scheduleUpdate();
+  async start() {
+    Log.info(
+      `Starting module: ${this.name} with identifier: ${this.identifier}`
+    );
+
+    // Read all words
+    await this.getWords();
+
+    // Set new word every x (nextWordInterval) milliseconds
+    setInterval(() => {
+      this.setNewWordpair();
+    }, this.config.nextWordInterval);
+
+    // If "alternating": Switch every 30 seconds between own language word and foreign language word
+    if (this.config.showpair === "alternating") {
+      this.showOwnWord = true;
+      setInterval(() => {
+        this.toogleOwnForeignWord();
+      }, this.config.toggleInterval);
+    }
   },
 
-  toOrFrom() {
-    this.sendSocketNotification("TO-OR-FROM", this.config);
-  },
+  toogleOwnForeignWord() {
+    this.showOwnWord = !this.showOwnWord;
+    if (this.showOwnWord) {
+      this.text = `<span style='visibility:hidden'>${this.ownLanguageWord}</span>
+                   <span style='visibility:visible'>${this.ownLanguageWord}</span>`;
+    } else {
+      this.text = `<span style='visibility:visible'>${this.foreignLanguageWord}</span>
+                   <span style='visibility:hidden'>${this.foreignLanguageWord}</span>`;
+    }
 
-  toAndFrom() {
-    this.sendSocketNotification("TO-AND-FROM", this.config);
+    this.updateDom();
   },
 
   // Process Data that was read from the file.
-  processData(data) {
-    // Split the data into lines
-    const lines = data.replace(/\n+$/, "").split("\n");
-
-    this.foreignl = [];
-    this.ownl = [];
-
-    for (const line of lines) {
-      const semicolonIndex = line.indexOf(";");
-      const foreignWord = line.substring(0, semicolonIndex);
-      const ownWord = line.substring(semicolonIndex + 1, line.length);
-
-      this.foreignl.push(foreignWord);
-      this.ownl.push(ownWord);
-    }
-
-    this.loaded = true;
-    const x = Math.floor(Math.random() * lines.length);
-    this.config.text = this.ownl[x];
-    this.config.translatedtext = this.foreignl[x];
+  setNewWordpair() {
+    const randomNumber = Math.floor(
+      Math.random() * this.ownLanguageArray.length
+    );
+    this.ownLanguageWord = this.ownLanguageArray[randomNumber];
+    this.foreignLanguageWord = this.foreignLanguageArray[randomNumber];
 
     if (this.config.showpair === "showboth") {
-      setInterval(() => {
-        this.toAndFrom();
-      }, 3000);
-    } else {
-      setInterval(() => {
-        this.toOrFrom();
-      }, 3000);
+      const wordpair = `${this.ownLanguageWord} - ${this.foreignLanguageWord}`;
+      this.text = wordpair;
+      this.updateDom();
     }
   },
 
-  // getWord from the file
-  async getWord() {
+  // getWords from the file
+  async getWords() {
     const response = await fetch(
       this.file(`languagefiles/${this.config.language}.csv`)
     );
 
     if (response.status === 200) {
       const data = await response.text();
-      this.processData(data);
+      const words = data.replace(/\n+$/, "").replaceAll(/\r/g, "").split("\n");
+
+      this.foreignLanguageArray = [];
+      this.ownLanguageArray = [];
+
+      words.forEach((line) => {
+        const semicolonIndex = line.indexOf(";");
+        const foreignWord = line.substring(0, semicolonIndex);
+        const ownWord = line.substring(semicolonIndex + 1);
+
+        this.foreignLanguageArray.push(foreignWord);
+        this.ownLanguageArray.push(ownWord);
+      });
+
+      this.setNewWordpair();
     } else {
       throw new Error("Failed to fetch language file.");
     }
   },
 
-  scheduleUpdate(delay) {
-    let nextWord = this.config.nextWordInterval;
-    if (typeof delay !== "undefined" && delay >= 0) {
-      nextWord = delay;
-    }
-
-    const self = this;
-    self.getWord();
-    setTimeout(function () {
-      self.scheduleUpdate();
-    }, nextWord);
-  },
-
   // Override dom generator.
   getDom() {
-    let wrapper = document.getElementById("wordpair");
-    if (!wrapper) {
-      wrapper = document.createElement("div");
-      wrapper.className = this.config.wordpaircssclassname;
-      wrapper.id = "wordpair";
+    const wrapper = document.createElement("div");
+    wrapper.className = this.config.wordpaircssclassname;
+    if (this.text) {
+      wrapper.innerHTML = this.text;
     }
+    wrapper.id = "wordpair";
+
     return wrapper;
   },
 
   getHeader() {
-    return this.translate(`LEARN ${this.config.language}`);
-  },
-  /* socketNotificationReceive(notification)
-   * used to get communication from the nodehelper
-   *
-   * argument notification object - status label from nodehelper.
-   */
-  socketNotificationReceived(notification, payload) {
-    const tofrom = document.getElementById("wordpair");
-
-    if (notification === "LEFT") {
-      tofrom.innerHTML = `<span style='visibility:visible'>${payload}</span>
-                          <span style='visibility:hidden'>${payload}</span>`;
-    } else if (notification === "RIGHT") {
-      tofrom.innerHTML = `<span style='visibility:hidden'>${payload}</span>
-                          <span style='visibility:visible'>${payload}</span>`;
-    } else if (notification === "BOTH") {
-      tofrom.innerHTML = payload;
-    }
+    return this.translate(
+      `${this.translate("LEARN")} ${this.translate(this.config.language)}`
+    );
   }
 });
--- README.md
+++ README.md
@@ -15,20 +15,27 @@ git clone https://github.com/thekampany/MMM-learnlanguage
 
 ## Using the module
 
-This module shows a word of a language you want to learn, alternating with the meaning in english. Train yourself.
+This module shows a word of a language you want to learn, alternating with the meaning in English. Train yourself.
 You will have to learn the grammar of the language somewhere else. This just helps in getting to know a set of commonly used words.
-The set of commonly used words is stored in a csv file per language.
+The set of commonly used words is stored in a CSV file per language.
 
 ## Config options
 
 | option                 | description                                                                                                                                                                  |
 | ---------------------- | ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
 | `language`             | `"danish"`, `"esperanto"`,`"frisian"`, `"german"`, `"italian"`, `"japanese"`, `"spanish"` or `"swedish"`. Has to match the name of the CSV file that contains the wordpairs. |
-| `nextWordInterval`     | In miliseconds. The interval between wordpairs. One word per day = `24*3600*1000`. Two words per day is `12*3600*1000`.                                                      |
-| `showpair`             | `"showboth"` or `"alternating"`                                                                                                                                              |
+| `nextWordInterval`     | In miliseconds. The interval between wordpairs.<br>Example: `24*60*60*1000` (eyery 24 hours).<br>Default: `12*60*60*1000` (every 12 hours).                                  |
+| `showpair`             | `"showboth"` or `"alternating"`<br>Default: `alternating`                                                                                                                    |
+| `toggleInterval`       | In miliseconds. Toggle time between own language word and foreign language word.<br>Only relevant if `"showpair": "alternating"`.<br>Default: `10*1000` (every 10 seconds).  |
 | `wordpaircssclassname` | Influences the fontsize, uses the CSS from main CSS.                                                                                                                         |
 
-## Example configuration to put in config.js
+## Configuration
+
+Here are two examples for an entry in `config.js`:
+
+### Simple example with wordpairs
+
+**Spanish - new wordpair every 12 hours**
 
 ```js
   {
@@ -36,9 +43,40 @@ The set of commonly used words is stored in a csv file per language.
     position: "top_right",
     config: {
       language: "spanish",
-      nextWordInterval: 90000,
+      nextWordInterval: 12*60*60*1000,
       showpair: "showboth",
       wordpaircssclassname: "bright medium"
+    }
+  },
+```
+
+### Two instaces with alternating words
+
+If multiple languages are learned in your household, you can also run multiple instances of the module by adding the module settings multiple times into the `config.js`.
+
+**German and Esperanto - new word every 24 hours - words are shown alternating every 5 seconds**
+
+```js
+  {
+    module: "MMM-learnlanguage",
+    position: "top_right",
+    config: {
+      language: "german",
+      nextWordInterval: 24*60*60*1000,
+      showpair: "alternating",
+      toggleInterval: 5*1000,
+      wordpaircssclassname: "bright large"
+    }
+  },
+  {
+    module: "MMM-learnlanguage",
+    position: "top_right",
+    config: {
+      language: "esperanto",
+      nextWordInterval: 24*60*1000,
+      showpair: "alternating",
+      toggleInterval: 5*1000,
+      wordpaircssclassname: "bright large"
     }
   },
 ```
@@ -46,4 +84,3 @@ The set of commonly used words is stored in a csv file per language.
 ## ToDo
 
 - Use a translation API in order to be able to translate from any to any language.
-- Influence the interval showing foreignlanguage vs ownlanguage (english).
